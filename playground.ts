7f6777cc49153d5124db9245c98393e7e9186151
Alex Rojas <alex.olmedo.rojas@gmail.com>
2022-04-10 07:51:52 -0500

    Add logPersonInfo2 and Person interface

--- playground.ts
+++ playground.ts
@@ -1,6 +1,21 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+// type Person = {
+//   name: string;
+//   age: number;
+// };
+
 export default function play() {
   const name: string = "Alex";
   const age: number = 26;
+
+  const person = {
+    name: "Kenny",
+    age: 5,
+  };
 
   function logPersonInfo(personName: string, personAge: number) {
     // const info = "Name: " + personName + " Age: " + personAge;
@@ -9,5 +24,13 @@ export default function play() {
     return info;
   }
 
+  function logPersonInfo2(person: Person) {
+    // const info = "Name: " + personName + " Age: " + personAge;
+    const info = `Name: ${person.name}, Age: ${person.age}`;
+    console.log(info);
+    return info;
+  }
+
   logPersonInfo(name, age);
+  logPersonInfo2(person);
 }
