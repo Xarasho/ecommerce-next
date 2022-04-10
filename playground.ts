4055b587c14129033d1d31a79374ba9b589f5eb0
Alex Rojas <alex.olmedo.rojas@gmail.com>
2022-04-10 07:38:06 -0500

    Add logPersonInfo function

--- playground.ts
+++ playground.ts
@@ -1,3 +1,13 @@
 export default function play() {
-  console.log("Hello world!");
+  const name: string = "Alex";
+  const age: number = 26;
+
+  function logPersonInfo(personName: string, personAge: number) {
+    // const info = "Name: " + personName + " Age: " + personAge;
+    const info = `Name: ${personName}, Age: ${personAge}`;
+    console.log(info);
+    return info;
+  }
+
+  logPersonInfo(name, age);
 }
